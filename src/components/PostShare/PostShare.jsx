7c0fe28b18072786dca5037936019b5420cb4daa
Sulav Adhikari <sulavadhikari17@gmail.com>
2023-01-10 12:27:24 +1100

    image upload design done

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React,{useState,useRef} from "react";
 import "./PostShare.css";
 import profileImg from "../../img/profileImg.jpg";
 import { UilScenery } from "@iconscout/react-unicons";
 import { UilPlayCircle } from "@iconscout/react-unicons";
 import { UilLocationPoint } from "@iconscout/react-unicons";
 import { UilSchedule } from "@iconscout/react-unicons";
+import { UilTimes } from "@iconscout/react-unicons";
+
 
 export const PostShare = () => {
+  const [image, setImage] = useState(null)
+  const imageRef = useRef()
+  const handleOnChange=(e)=>{
+   if(e.target.files && e.target.files[0]){
+    let img = e.target.files[0]
+    setImage({
+      image:URL.createObjectURL(img)
+    })
+   }
+  }
   return (
     <div className="PostShare">
       <img src={profileImg} alt="" />
@@ -14,7 +26,7 @@ export const PostShare = () => {
         <input type="text" placeholder="What's happening" />
         <div className="postOptions">
           <div className="option"
-          style={{color:"var(--photo)"}}>
+          style={{color:"var(--photo)"}} onClick={()=>imageRef.current.click()}>
             <UilScenery />
             Photo
           </div>
@@ -33,7 +45,16 @@ export const PostShare = () => {
           <button className="btn ps-button">
             Share
           </button>
+          <div style={{display:"none"}}>
+            <input type="file" name="image" ref={imageRef} onChange={handleOnChange}/>
+          </div>
         </div>
+        {image &&(
+          <div className="previewImage">
+            <UilTimes style={{cursor:"pointer"}} onClick={()=>setImage(null)} />
+            <img src={image.image} alt="" />
+          </div>
+        )}
       </div>
     </div>
   );
